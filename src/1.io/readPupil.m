ae1c615399c994730dac865a06250839f91e6180
Cameron Holman <cameron.holman.ny@gmail.com>
2020-02-13 17:45:40 -0800

    Reorg and Return readPupil to orig

--- src/1.io/readPupil.m
+++ src/1.io/readPupil.m
@@ -44,69 +44,111 @@ function pupils = readPupil(datDir)
         
     %} 
     
-        % Set up base struct
-        %pupils = struct()
-    
-        %% Read in pupilData.mat files =
-        pp = struct2cell(dir([datDir, '**/*pupilData.mat']));
-        A = size(pp,2);
+    % Set up base struct
+    %pupils = struct()
 
-        % Read through each CSV
-        for kk = 1:A
-            eFile = pp{1,kk};
-            fName0 = eFile(1:end-4);
-            f0 = [pp{2,kk}, '/', pp{1,kk}];
+    %% Read in pupilData.mat files =
+    pp = struct2cell(dir([datDir, '**/*pupilData.mat']));
+    A = size(pp,2);
 
-            if ~exist(f0,'file')
-                disp(f0);
-                disp('File does not exist');
-                continue
-            end
-            
-            fprintf('Currently reading in csv file: %s\n',fName0)
-            
-            tmp = load(f0);
-            pd = tmp.Pupildata;
-            %pupils{kk}  = pup0
-            
+    %% Read through each CSV and extract data
+    for kk = 1:A
+        eFile = pp{1,kk};
+        fName0 = eFile(1:end-4);
+        f0 = [pp{2,kk}, '/', pp{1,kk}];
 
+        if ~exist(f0,'file')
+            disp(f0);
+            disp('File does not exist');
+            continue
+        end
+        
+        fprintf('Currently reading in csv file: %s\n',fName0)
+        
+        tmp = load(f0);
+        pd = tmp.Pupildata;
+        %pupils{kk}  = pup0
+        
 
+        %% Pull data from each frame
             % Iterate through each frame and add that frames
             % data to the correct org structure, nested in one
             % cell of a cell array within strucure
             % The fist order cell referes to a trial
             % each contained cell referrs to a frame within that 
             % trial
+        
+        B = length (pd);
+
+        for zz = 1:B
+            cframe = pd{zz};
+            if isfield(cframe, 'rotated_rect')
+                pupils.rotated_rect{kk}{zz} = cframe.rotated_rect;
+            end
             
-            B = length (pd);
+            if isfield(cframe, 'center')
+                pupils.center{kk}{zz} = cframe.center;
+            end
+            
+            if isfield(cframe, 'major_r')
+                pupils.major_r{kk}{zz} = cframe.major_r;
+            end
 
-            for zz = 1:B
-                cframe = pd{zz};
-                if isfield(cframe, 'rotated_rect')
-                    pupils.rotated_rect{kk}{zz} = cframe.rotated_rect;
-                end
-                
-                if isfield(cframe, 'center')
-                    pupils.center{kk}{zz} = cframe.center;
-                end
-                
-                if isfield(cframe, 'major_r')
-                    pupils.major_r{kk}{zz} = cframe.major_r;
-                end
+            if isfield(cframe, 'frame_id')
+                pupils.frame_id{kk}{zz} = cframe.frame_id;
+            end
 
-                if isfield(cframe, 'frame_id')
-                    pupils.frame_id{kk}{zz} = cframe.frame_id;
-                end
+            if isfield(cframe, 'frame_intensity')
+                pupils.frame_intensity{kk}{zz} = cframe.frame_intensity;
+            end
 
-                if isfield(cframe, 'frame_intensity')
-                    pupils.frame_intensity{kk}{zz} = cframe.frame_intensity;
-                end
-
-                if isfield(cframe, 'contour')
-                    pupils.contour{kk}{zz} = cframe.contour;
-                end
+            if isfield(cframe, 'contour')
+                pupils.contour{kk}{zz} = cframe.contour;
             end
 
         end
+
+        %{
+                cmh 200213.2
+                Note: Will convert major_r within analyze pupil as it needs to be vectorized for 
+                interpolation and low pass filter. The others do not need to be and are likely 
+                not to be used anyway. Removing this section from readPupil and adding necessary 
+                to analyzePupil, i.e just vectorization, interpolation, and low pass of 
+                major_r
         
+        %% Convert all cell arrays into vectors
+            %{
+                cmh 200213.1
+                With all of the data transferred, it will be in the form of 
+                a cell array. This is inconsistent with other data and should be
+                converyed to a vector. All empty cells will be replaces with NaNs
+
+                Interpolation and low pass filtering will be within another function
+                analyzePupil
+            %}
+        
+        % by iteration
+        fn = fieldnames(pupils)
+        for jj = 1:numel(fn)
+            cur = pupils.(fn{jj}){kk}
+
+            %%%%%%%%%%%%%%%%%%%
+            disp('________________tst___________')
+            disp(fn)
+            disp(jj)
+
+            disp(cur)
+
+
+
+            %%%%%%%%%%%%%%%%%%%
+
+            curEmptyIndex = cellfun('isempty', cur)
+            cur(curEmptyIndex)={NaN}
+            curVec = cell2mat(cur)        
+            pupils.(fn{jj}){kk} = curVec
+        end
+        %}
     end
+    
+end
